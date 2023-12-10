eab415fbcf254d7f23089f3dc009c67add57e32e
omertayhan <omertufan2000@gmail.com>
2023-12-11 01:57:18 +0300

    chart implementation

--- src/screens/CoinDetailedScreen/index.jsx
+++ src/screens/CoinDetailedScreen/index.jsx
@@ -1,8 +1,13 @@
 import react from "react";
-import { View, Text } from "react-native";
+import { View, Text, Dimensions } from "react-native";
 import Coin from "../../../assets/data/crypto.json";
 import CoinDetailedHeader from "./comnponents/CoinDetailedHeader";
 import { AntDesign } from "@expo/vector-icons";
+import {
+  ChartDot,
+  ChartPath,
+  ChartPathProvider,
+} from "@rainbow-me/animated-charts";
 
 import styles from "./styles";
 
@@ -11,6 +16,7 @@ const CoinDetailedScreen = () => {
     image: { small },
     name,
     symbol,
+    prices,
     market_data: {
       market_cap_rank,
       current_price,
@@ -19,39 +25,52 @@ const CoinDetailedScreen = () => {
   } = Coin;
   const percentageColor =
     price_change_percentage_24h < 0 ? "#ea3943" : "#16c784";
-
+  const screenWidth = Dimensions.get("window").width;
   return (
     <View style={{ paddingHorizontal: 10 }}>
-      <CoinDetailedHeader
-        image={small}
-        symbol={symbol}
-        market_cap_rank={market_cap_rank}
-      />
-      <View style={styles.priceContainer}>
-        <View>
-          <Text style={styles.name}> {name} </Text>
-          <Text style={styles.currentPrice}> ${current_price.usd} </Text>
+      <ChartPathProvider
+        data={{
+          points: prices.map((price) => ({ x: price[0], y: price[1] })),
+          smoothingStrategy: "bezier",
+        }}
+      >
+        <CoinDetailedHeader
+          image={small}
+          symbol={symbol}
+          market_cap_rank={market_cap_rank}
+        />
+        <View style={styles.priceContainer}>
+          <View>
+            <Text style={styles.name}> {name} </Text>
+            <Text style={styles.currentPrice}> ${current_price.usd} </Text>
+          </View>
+          <View
+            style={{
+              backgroundColor: percentageColor,
+              paddingHorizontal: 3,
+              paddingVertical: 8,
+              borderRadius: 5,
+              flexDirection: "row",
+            }}
+          >
+            <AntDesign
+              name={price_change_percentage_24h < 0 ? "caretdown" : "caretup"}
+              size={12}
+              color={"white"}
+              style={{ alignSelf: "center", marginRight: 5 }}
+            />
+            <Text style={styles.priceChange}>
+              {price_change_percentage_24h.toFixed(2)}%
+            </Text>
+          </View>
         </View>
-        <View
-          style={{
-            backgroundColor: percentageColor,
-            paddingHorizontal: 3,
-            paddingVertical: 8,
-            borderRadius: 5,
-            flexDirection: "row",
-          }}
-        >
-          <AntDesign
-            name={price_change_percentage_24h < 0 ? "caretdown" : "caretup"}
-            size={12}
-            color={"white"}
-            style={{ alignSelf: "center", marginRight: 5 }}
-          />
-          <Text style={styles.priceChange}>
-            {price_change_percentage_24h.toFixed(2)}%
-          </Text>
-        </View>
-      </View>
+        <ChartPath
+          height={screenWidth / 2}
+          stroke="yellow"
+          width={screenWidth}
+        />
+        <ChartDot style={{ backgroundColor: "blue" }} />
+      </ChartPathProvider>
     </View>
   );
 };
